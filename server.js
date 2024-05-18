19f636c4f17aeae32fbf0451d9fa6d74d40392e1
Javeed Abdul Wahhab <javeedw@192-168-1-162.tpgi.com.au>
2024-05-18 11:12:29 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express Updated v2!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/api/message', (req, res) => {
+  res.json({ message: 'Hello from Express API Updated!' });
+});
+
